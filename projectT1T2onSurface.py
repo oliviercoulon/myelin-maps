2e309897d4de8f73dc6efaf86dfd09b62246aadc
olivier <olivier.coulon@univ-amu.fr>
2020-04-02 10:20:53 +0200

    Implemented the main function.

--- projectT1T2onSurface.py
+++ projectT1T2onSurface.py
@@ -1,3 +1,21 @@
 from soma import aims
 import numpy as np
 
+
+def projectOnSurface()
+
+
+def main(arguments):
+    if len(arguments)==3:
+        myelinN=arguments[1]
+        surfaceN=arguments[2]
+        r=aims.Reader()
+
+        projectOnSurface(myelin, surface)
+    else:
+        print 'Usage:'
+        print 'python projectT1T2onSurface.py myelinImage surface'
+
+if __name__ == "__main__":
+     arguments=sys.argv
+     main(arguments)
